4ce159dc35ca170c0abf99e067cbcf108e046e7a
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-05 01:04:57 +0300

    add functionWithArgs

--- freecodecamp.js
+++ freecodecamp.js
@@ -6,3 +6,13 @@ function reusableFunction () {
 }
 
 reusableFunction();
+
+
+// Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+// Call the function with two numbers as arguments.
+
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(3,4);
