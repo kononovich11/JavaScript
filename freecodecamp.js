6fec31b1fbc6ada513eb2acd5c1e3f7e3de5bbb4
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-05 09:26:13 +0300

    use global variable

--- freecodecamp.js
+++ freecodecamp.js
@@ -22,3 +22,13 @@ functionWithArgs(3,4);
 function timesFive(num) {
   return num * 5;
 }
+
+// Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
+// Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
+
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
