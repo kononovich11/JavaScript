17c43aa073db64e961bd338de7fc8cf3fb28da56
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-06 22:32:38 +0300

    convert to string

--- codewars.js
+++ codewars.js
@@ -10,3 +10,8 @@ function opposite(number) {
 function squareOrSquareRoot(array) {
   return array.map(item => Number.isInteger(Math.sqrt(item)) ? Math.sqrt(item) : item ** 2);
 }
+
+// Convert a Number to a String
+function numberToString(num) {
+  return `${num}`;
+}
