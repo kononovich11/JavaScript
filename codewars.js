2ae34116337b5fa108589dd833803dfa0877847d
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-06 22:25:33 +0300

    add squareOrSquareRoot and opposite functions

---
+++ codewars.js
@@ -0,0 +1,12 @@
+// Opposite number
+function opposite(number) {
+  return -number;
+}
+
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+// Return a new array with processing every number of the input-array like this:
+// If the number has an integer square root, take this, otherwise square the number.
+
+function squareOrSquareRoot(array) {
+  return array.map(item => Number.isInteger(Math.sqrt(item)) ? Math.sqrt(item) : item ** 2);
+}
