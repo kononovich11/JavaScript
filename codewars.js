c8beb8438697ddfb7b889c7114e6ba79e2fb6489
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-26 23:24:54 +0300

    add litres funtion

--- codewars.js
+++ codewars.js
@@ -61,3 +61,8 @@ class SmallestIntegerFinder {
 function noSpace(x){
   return x.split('').filter(item => item !== ' ').join('');
 }
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
