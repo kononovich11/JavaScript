53411564c3851b4d708be9f0ae993fb111d4c081
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-26 23:32:51 +0300

    Reversed Words

--- codewars.js
+++ codewars.js
@@ -66,3 +66,8 @@ function noSpace(x){
 function litres(time) {
   return Math.floor(time * 0.5);
 }
+
+// Reversed Words
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
