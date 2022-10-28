aa7a5073510dde640cefaca2b3c1da34ff2cddb1
kononovich11 <60875318+kononovich11@users.noreply.github.com>
2022-10-28 17:42:38 +0300

    create short+long+short function

--- codewars.js
+++ codewars.js
@@ -105,3 +105,12 @@ function monkeyCount(n) {
   }
   return arr; 
 }
+
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside.
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
+
+function solution(a, b){
+  return a.length < b.length? `${a+b+a}` :`${b+a+b}`;
+}
+
